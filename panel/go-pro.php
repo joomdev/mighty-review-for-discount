2d385ccfb7beedf197c62f893616e9d5202ab3e4
actuallyakash <akash_gupta@hotmail.com>
2021-06-02 14:33:49 +0530

    Updated logos and go-pro page style

--- panel/go-pro.php
+++ panel/go-pro.php
@@ -11,13 +11,18 @@ include_once MIGHTY_RFD_DIR_PATH . 'includes/header.php';
 
 <div class="mighty-go-pro-page">
 
-<p class="basic-desc">You are currently on a <strong>Free Version</strong></p>
-<p class="sub-desc">Limited features available in free version.</p>
 
-<hr style="margin: 40px 0;">
+
+
 
 <div class="pro-details">
     <div class="pro-desc">
+
+        <p class="basic-desc">You are currently on a <strong>Free Version</strong></p>
+        <p class="sub-desc">Limited features available in free version.</p>
+
+        <hr style="margin: 40px 0;">
+
         <p class="basic-desc">Need more features?</strong></p>
         <p class="sub-desc">Updgrade now for more cool and other advanced features.</p>
         
@@ -28,6 +33,9 @@ include_once MIGHTY_RFD_DIR_PATH . 'includes/header.php';
             <li><span class="dashicons dashicons-saved"></span> Lorem ipsum dolor site amet..</li>
             <li><span class="dashicons dashicons-saved"></span> Lorem ipsum dolor site amet..</li>
         </ul>
+    </div>
+    <div class="pro-illustration">
+        <img src="<?php echo MIGHTY_RFD_PLG_URL . 'assets/images/pro-illustration.png'; ?>" alt="Mighty RFD Pro Illustration">
     </div>
 </div>
 
